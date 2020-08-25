11015dbee5e47e7207994f899dbf8de0b00ecadd
Alex <alexdinu1@hotmail.com>
2020-08-25 11:00:23 -0500

    Added new rest test.

--- src/main/java/com/alex/shoppingcart/model/cart/CartModel.java
+++ src/main/java/com/alex/shoppingcart/model/cart/CartModel.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
+
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(collection = "cart")
 public class CartModel {
